29ef6ffa61c9430f8e0ef472fb2d44a3258b0056
Michał Kuliga <mkuliga72@gmail.com>
2023-11-19 13:53:48 +0100

    End of section 6 + 7.1,7.2

--- API/Program.cs
+++ API/Program.cs
@@ -1,10 +1,4 @@
-using System.Text;
-using API;
-using API.Data;
 using API.Extensions;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
